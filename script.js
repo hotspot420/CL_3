b552d7209ffcc30d037157210e1d482a4cf19d82
hotspot420 <rc5570@nyu.edu>
2024-09-18 00:43:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('part1').addEventListener('click', function() {
+    document.getElementById('story').innerText += "\nYou open your eyes to find yourself in the back of a classroom in 2008. ";
+});
+
+document.getElementById('part2').addEventListener('click', function() {
+    document.getElementById('story').innerText += " You've just exhaled your bong rip of salvia, having lived 16 years in a 10 minute vortex.";
+});
